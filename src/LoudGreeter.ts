17e8edfb31a7f68dbf72865cc90d8988a1ae47fb
ramirolynch <ramirolynch@yahoo.com>
2022-01-13 17:47:26 -0600

    removed comments

--- src/LoudGreeter.ts
+++ src/LoudGreeter.ts
@@ -1,11 +1,4 @@
-// LoudGreeter Class
-// In its own file, create a class named LoudGreeter. This is a subclass of Greeter.
-// Additional Properties:
-// extra (a string): Use the private modifier. This property keeps track of the extra exclamation points. Hard code an initial value.
-// Additional Methods:
-// addVolume: No parameters. Returns void. Every time this is called, it adds an additional exclamation point to extra.
-// Override greet: Override the greet method to add the extra exclamation points to the end. For example, greet might return "Hello, Grant!!!". Note, the original greet method already has one exclamation point, so LoudGreeter starts with two exclamation points even before calling addVolume.
-// Jest Tests: Test with and without calling addVolume. Test calling addVolume different numbers of times.
+
 
 import { Greeter} from "./greeter";
 
@@ -28,7 +21,7 @@ export let greetText = new LoudGreeter('Hello')
 
 greetText.addVolume()
 
-let addVolume = greetText.greet('Rami')
+export let addVolume = greetText.greet('Rami')
 
 
 console.log(addVolume)
